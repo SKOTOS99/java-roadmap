ef172f6819f38b6894118415b3fa44ea967da2dd
SKOTOS99 <felipd95@hotmail.com>
2024-11-04 17:17:44 -0600

    estructura de datos: uso de streams y listas

---
+++ src/main/java/com/roadmap/java/readmap/fundaments/ListasYarreglos.java
@@ -0,0 +1,47 @@
+package com.roadmap.java.readmap.fundaments;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ListasYarreglos {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el valor de la lista: ");
+        String numeros = sc.nextLine();
+        List<Integer> arrayNumeros = new ArrayList<>(Arrays.stream(numeros.split(",")).map(Integer::valueOf).toList());
+        StringBuilder result = new StringBuilder();
+        result.append("Lista ingresada: ").append(arrayNumeros).append("\n").append("\n")
+                .append("Valor minimo: ").append(calculeMin(arrayNumeros)).append("\n")
+                .append("Valor maximo: ").append(calculeMax(arrayNumeros)).append("\n")
+                .append("Valor promedio: ")
+                .append(calculeSum(arrayNumeros)).append("\n")
+                .append("Lista ordenada ASC: ").append(orderAsc(arrayNumeros)).append("\n")
+                .append("Lista ordenada DESC: ").append(orderDesc(arrayNumeros)).append("\n");
+        System.out.println(result);
+
+    }
+
+    public static int calculeMax(List<Integer> list){
+        return list.stream().max(Integer::compareTo).get();
+    }
+
+    public static int calculeMin(List<Integer> list){
+        return list.stream().min(Integer::compareTo).get();
+    }
+
+    public static int calculeSum(List<Integer> list){
+        return (list.stream().reduce(0, Integer::sum))/list.size();
+    }
+
+    public static List<Integer> orderAsc(List<Integer> list){
+        return list.stream().sorted().toList();
+    }
+
+    public static List<Integer> orderDesc(List<Integer> list){
+        return list.stream().sorted(Comparator.reverseOrder()).toList();
+    }
+}
